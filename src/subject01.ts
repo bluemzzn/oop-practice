b8cab095fb7e4af1fa78609ccff24011b73ab4c7
Kuljira Namjai <synthiawithsyren@gmail.com>
2026-01-19 23:35:25 +0700

    subject01

--- src/subject01.ts
+++ src/subject01.ts
@@ -12,26 +12,33 @@ interface ObjectWant {
     title: string;
 }
 
+// U have to write the filter before return the result and use the filterTitle variable in return value
+
 export async function getShortPosts(): Promise<ObjectWant[]> {
     try {
         const res = await axios.get<Posts[]>("https://jsonplaceholder.typicode.com/posts");
         const posts = res.data;
 
-        if(posts.length < 15){
+        const filterTitle =  posts.filter(post => post.title.length < 15);
+        if(filterTitle.length === 0){
             return [];
         }
+
         /* if u want to console.log to check the result 
             const shortPost = posts.map().....
             console.log(shortPost)
             return shortPost;
+
+            or new method(easier)
+            call function --- getShor   tPosts().then(result => console.log(result));
         */
        
-        return posts.map(post => ({ id : post.id, title : post.title}));
+        return filterTitle.map(post => ({ id : post.id, title : post.title}));
         
     } catch (error) {
-        throw new Error("Cannot Fetch Posts");
+        return [];
     }
 }
 
 
-getShortPosts().then(result => console.log(result));
+// getShortPosts().then(result => console.log(result));
