a94b312fc9f750b7485d520e802c0f5b2fa1fc2e
Kuljira Namjai <synthiawithsyren@gmail.com>
2026-01-20 11:46:16 +0700

    subject05.ts

---
+++ src/subject05.ts
@@ -0,0 +1,44 @@
+// https://jsonplaceholder.typicode.com/users
+
+import axios from "axios";
+
+/* "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      no geo
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }*/
+
+interface User {
+    id: number;
+    name: string;
+    address: {
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: string;
+    };
+}
+interface UserAddress {
+    id: number;
+    name: string;
+    fullAddress: string;
+}
+
+export async function getUserAddress(): Promise<UserAddress[]> {
+    try {
+        const res = await axios.get<User[]>("https://jsonplaceholder.typicode.com/users");
+        const userdata = res.data;
+        // u don't have to create more variable just use template literal in map function instead.
+        return userdata.map(user => ({ id: user.id, name: user.name, fullAddress: `${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}` }));
+    }
+    catch (error) {
+        return [];
+    }
+}
+
+getUserAddress().then(check => console.log(check));
